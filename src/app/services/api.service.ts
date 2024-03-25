017588dd08a255163123af5c5e2b5b252a72cec6
santokie21 <officialuse3012@gmail.com>
2024-03-25 17:44:10 +0530

    services implemented

--- src/app/services/api.service.ts
+++ src/app/services/api.service.ts
@@ -1,9 +1,18 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { Options } from '../../types';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ApiService {
 
-  constructor() { }
+  constructor(
+    private http: HttpClient
+  ) { }
+
+  get<T>(url: string, options: Options): Observable<T> {
+    return this.http.get<T>(url, options) as Observable<T>;
+  }
 }
--- src/app/services/products.service.ts
+++ src/app/services/products.service.ts
@@ -1,9 +1,21 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { ApiService } from './api.service';
+import { PaginationParams, Products } from '../../types';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ProductsService {
 
-  constructor() { }
+  constructor(private apiService: ApiService) { }
+
+  getProducts = (url: string, params: PaginationParams): Observable<Products> => {
+    return this.apiService.get(url, {
+      params,
+      responseType: 'json',
+
+    });
+  }
 }
